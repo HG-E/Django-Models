a29cd8795012c555ec8fa83525a1752d183688c0
HILLARY GOODNESS <goodek1991@gmail.com>
2022-06-23 09:35:03 +0100

    update

--- echefula/Blog/models.py
+++ echefula/Blog/models.py
@@ -1,6 +1,7 @@
 from turtle import title
 from django.db import models
 from django.contrib.auth import get_user_model
+
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=200)
